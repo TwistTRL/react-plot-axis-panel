56441d3746b93e5104c437df5ce6f0414501b462
Lingyu Zhou <zhoulytwin@gmail.com>
2019-06-14 14:29:09 -0400

    Updates

---
+++ precache-manifest.e1cbf1a1b45f6ada35a751788e00caca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5baa2d859dc7aff84805bdd7bdc9cf",
+    "url": "/react-plot-axis-panel/index.html"
+  },
+  {
+    "revision": "4a97c0a4e400bb30274c",
+    "url": "/react-plot-axis-panel/static/css/main.9d313dd0.chunk.css"
+  },
+  {
+    "revision": "4cf0b8e9d1392a3f00d4",
+    "url": "/react-plot-axis-panel/static/js/2.24a432ae.chunk.js"
+  },
+  {
+    "revision": "4a97c0a4e400bb30274c",
+    "url": "/react-plot-axis-panel/static/js/main.25e48cd0.chunk.js"
+  },
+  {
+    "revision": "1f466664eb1f7130057a",
+    "url": "/react-plot-axis-panel/static/js/runtime~main.63acfe64.js"
+  }
+]);
